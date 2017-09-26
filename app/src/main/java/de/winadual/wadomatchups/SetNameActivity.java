9fb854189fdc225668a638d492082c2041d81f5a
Hyde46 <denis.heid@gmx.de>
2017-09-26 13:34:17 +0200

    Initial Commit

---
+++ app/src/main/java/de/winadual/wadomatchups/SetNameActivity.java
@@ -0,0 +1,13 @@
+package de.winadual.wadomatchups;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SetNameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_name);
+    }
+}
